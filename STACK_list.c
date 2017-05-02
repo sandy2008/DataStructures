70faec3deb708a02eac84c068be2f7ffb0c3c63a
sady2008 <admin@admin-no-iMac.local>
2017-05-02 11:19:53 +0900

    0502

---
+++ STACK_list.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "Item.h"
+#include "STACK.h"
+
+typedef struct STACKnode* link;
+
+struct STACKnode{
+    Item x;
+    link next;
+};
+
+static link head;
+
+
+void STACKinit(int maxN){
+    head = NULL;
+}
+
+int STACKempty(void){
+    return head == NULL;
+}
+
+Item STACKpop(void){
+    Item x = head->item;
+    link t = head->next;
+    free(head);
+    head = t;
+    return x;
+}
+
+link insertFirst(Item n, link hd){
+    link t = makeNew(n);
+    t->next = hd;
+    return t;
+}
+
+void STACKpush(Item x){
+    head = insertFirst(x, head);
+}
+
+
