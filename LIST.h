35cbf4d9580356bc3fa53fe9598cf3e82d0c343b
sady2008 <sandy2142@qq.com>
2017-05-02 11:35:40 +0900

    0502

---
+++ LIST.h
@@ -0,0 +1,8 @@
+typedef struct node *link;
+struct node { Item item; link next; };
+link LISTempty(void);
+Item LISThead(link);
+link LISTtail(link);
+link LISTcons(Item,link);
+int LISTlength(link);
+void LISTprint(link);
---
+++ LIST_ADT.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include "Item.h"
+#include "LIST.h"
+
+struct
