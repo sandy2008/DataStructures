f845e1f624b47f42559c8e84e76a398c8af32977
sady2008 <sandy2142@qq.com>
2017-05-02 19:35:26 +0900

    test

--- LIST_ADT.c
+++ LIST_ADT.c
@@ -2,4 +2,38 @@
 #include "Item.h"
 #include "LIST.h"
 
-struct
+static link head;
+
+link LISTempty(void){
+    return NULL;
+}
+
+Item LISThead(link hd){
+    if (hd == NULL)
+        return 0;
+    if()
+}
+
+link LISTtail(link){
+
+}
+
+link LISTcons(Item,link){
+}
+
+int LISTlength(link){
+}
+
+void LISTprint(link hd){
+    if(hd == NULL)
+        printf("No List");
+    else{
+         printf("[");
+         print("%d",hd->item);
+         while(hd->next != NULL){
+            hd = hd->next;
+            printf("%,d",hd->item);
+            printf("]\n");
+         }
+}
+}
