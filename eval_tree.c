a9e4d08b4ea3900723f44740c54950f90dda0955
Sandy Chen <sandy2142@qq.com>
2017-05-21 19:16:22 +0900

    hw3

---
+++ eval_tree.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include "BTREE.h"
+
+int ctoi(char c){ return (c - '0'); }
+int evalTree(BTlink tr){
+    if (tr->label==NULL)
+        return 0;
+    if (BTisLeaf(tr)==1)
+        return tr->label;
+    if (tr->label=='+')
+        return tr->l->label+tr->r->label;
+    if (tr->label=='-')
+        return tr->l->label-tr->r->label;
+    if (tr->label=='*')
+        return tr->l->label*tr->r->label;
+
+}
+void main(void){
+   BTlink treeOne = BTcons('1', NULL, NULL);
+   BTlink treeTwo = BTcons('2', NULL, NULL);
+   BTlink treeThree = BTcons('3', NULL, NULL);
+   BTlink treeFour = BTcons('4', NULL, NULL);
+   BTlink treeFive = BTcons('5', NULL, NULL);
+   BTlink treeRRR = BTcons('+', treeFour, treeFive);
+   BTlink treeRR = BTcons('*', treeThree, treeRRR);
+   BTlink treeR = BTcons('+', treeTwo, treeRR);
+   BTlink tree = BTcons('+', treeOne, treeR);
+   printf("result = %d\n", evalTree(tree)) ;
+}
