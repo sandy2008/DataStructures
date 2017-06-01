d3c1c78326ee92b4525021738fca862b655a750c
Sandy Chen <sandy2142@qq.com>
2017-06-02 00:57:54 +0900

    hw

--- quicksort2.c
+++ quicksort2.c
@@ -3,14 +3,17 @@ extern int c;
 
 void sort(Item a[],int left,int right){
     int i;
+    if(right - left >9){
     if (right <= left) return;
     i = partition(a,left,right);
     sort(a,left,i-1);
     sort(a,i+1,right);
+    }
+    else insertion(a,left,right);
 }
 
 int partition(Item a[],int left,int right){
-    Item pivot = a[right];
+    Item pivot = a[(left+right)/2];
     int i = left-1, j = right-1;
     while(1){
     while(less(a[++i],pivot));
@@ -25,4 +28,19 @@ int partition(Item a[],int left,int right){
     }
     exch(a[i], a[right]);
     return i;
+}
+
+void insertion(Item a[], int left, int right){
+    int i;
+    for (i = left + 1;i <= right; i++){
+        int j = i;
+        Item v = a[i];
+        c++;
+        while (less(v,a[j-1]) && left < j){
+            c++;
+            a[j] = a[j-1];
+            j--;
+        }
+        a[j] = v;
+        }
 }
