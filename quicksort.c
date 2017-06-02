3075d911fd22fe8a4b65340d9242ade5307af947
sady2008 <admin@admin-no-iMac.local>
2017-06-02 18:17:39 +0900

    hwfinal

--- quicksort.c
+++ quicksort.c
@@ -1,6 +1,27 @@
 #include "sort.h"
 extern int c;
 
+int partition(Item a[],int left,int right){
+    Item pivot = a[right];
+    int i = left-1, j = right-1;
+    while(1){
+    while(less(a[++i],pivot)) c++;
+    while(less(pivot,a[j])){
+        j--;
+	 c++;
+        if( i >= j )
+            break;
+    }
+    if ( i >= j )
+    break;
+    exch(a[i], a[j]);
+    c++;
+    }
+    c++;
+    exch(a[i], a[right]);
+    return i;
+}
+
 void sort(Item a[],int left,int right){
     int i;
     if (right <= left) return;
@@ -9,20 +30,4 @@ void sort(Item a[],int left,int right){
     sort(a,i+1,right);
 }
 
-int partition(Item a[],int left,int right){
-    Item pivot = a[right];
-    int i = left-1, j = right-1;
-    while(1){
-    while(less(a[++i],pivot));
-    while(less(pivot,a[j])){
-        j--;
-        if( i >= j )
-            break;
-    }
-    if ( i >= j )
-        break;
-    exch(a[i], a[j]);
-    }
-    exch(a[i], a[right]);
-    return i;
-}
+
--- quicksort2.c
+++ quicksort2.c
@@ -1,32 +1,25 @@
 #include "sort.h"
 extern int c;
 
-void sort(Item a[],int left,int right){
-    int i;
-    if(right - left >9){
-    if (right <= left) return;
-    i = partition(a,left,right);
-    sort(a,left,i-1);
-    sort(a,i+1,right);
-    }
-    else insertion(a,left,right);
-}
 
 int partition(Item a[],int left,int right){
     Item pivot = a[(left+right)/2];
     int i = left-1, j = right-1;
     while(1){
-    while(less(a[++i],pivot));
+    while(less(a[++i],pivot)) c++;
     while(less(pivot,a[j])){
         j--;
+        c++;
         if( i >= j )
             break;
     }
     if ( i >= j )
         break;
     exch(a[i], a[j]);
+    c++;
     }
     exch(a[i], a[right]);
+    c++;
     return i;
 }
 
@@ -44,3 +37,16 @@ void insertion(Item a[], int left, int right){
         a[j] = v;
         }
 }
+
+void sort(Item a[],int left,int right){
+    int i;
+    if(right - left >9){
+    if (right <= left) return;
+    i = partition(a,left,right);
+    sort(a,left,i-1);
+    sort(a,i+1,right);
+    }
+    else    
+	insertion(a,left,right);
+}
+
