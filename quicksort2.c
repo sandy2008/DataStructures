77c2a3cf79da17405580dd8ca8b0d57e35f4ab83
sady2008 <admin@admin-no-iMac.local>
2017-06-05 21:02:10 +0900

    finalver

--- quicksort2.c
+++ quicksort2.c
@@ -2,9 +2,9 @@
 extern int c;
 
 int partition(Item a[],int left,int right){
-    Item mid = a[(left+right)/2];
+    Item mid = (left+right)/2;
     int piv;
-    Item pivot = (a[left] < a[mid] ? (a[mid] < a[right] ? a[mid] : a[left
+    Item pivot = (a[left] < a[mid] ? (a[mid] < a[right] ? a[mid] : a[left]
  < a[right] ? a[right] : a[left]) : (a[mid] > a[right] ? a[mid] : a[left] > a[right] ? a[right]
 : a[left]));
     if(pivot == a[left])
